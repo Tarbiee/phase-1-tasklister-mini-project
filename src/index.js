15573ddba24a65d170f602a9f9c7ed7e0985e19d
Tarbiee <dorcusstaby@gmail.com>
2023-09-28 19:02:49 +0300

    completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector('#create-task-form');
+  const taskList = document.querySelector('#tasks')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();//set target to default
+    const valueInput = document.querySelector('#new-task-description').value;//get valueof target
+    const listItem = document.createElement('li');
+    listItem.textContent = valueInput;
+
+    const button = document.createElement('button')
+    button.textContent = 'x'
+    button.addEventListener('click',(e)=>{
+      listItem.remove();
+    });
+
+
+listItem.appendChild(button);
+taskList.appendChild(listItem);
+form.reset();
+
+
+  });
   // your code here
 });
+
